a123383d96a8b443121aeb52b1639a8a17739df1
hjldev <a2437463@outlook.com>
2023-07-03 09:59:05 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -7,8 +7,6 @@
  */
 const USER_CONFIG = {
 
-  USE_PASSAGE: 'server-chan',
-
   // 使用微信测试号：公众号APP_ID
   APP_ID: '',
 
@@ -16,16 +14,16 @@ const USER_CONFIG = {
   APP_SECRET: '',
 
   PROVINCE: '山东',
-  CITY: '滕州',
+  CITY: '枣庄',
 
   USERS: [
     {
       // 想要发送的人的名字
       name: 'me',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'SCT214744TUmEnJeenMeMAnVpnHn1U3niV',
+      id: 'o6NbM6QelHTcEQ71QJau-CoHL1sc',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '0001',
+      useTemplateId: 'ri7e3zZojKG9DekBDuoQKcY5qX9SR2jE5xZCHnfyAQg',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '09-07',
       festivals: [
@@ -47,13 +45,13 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '0002',
+  CALLBACK_TEMPLATE_ID: '1ZNZb2StgEpeJ8ROy7XXmo4efqP0klV5pXZXAjJOzag',
 
   CALLBACK_USERS: [
     {
       name: '自己',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'SCT214744TUmEnJeenMeMAnVpnHn1U3niV',
+      id: 'o6NbM6QelHTcEQ71QJau-CoHL1sc',
     }
   ],
 
